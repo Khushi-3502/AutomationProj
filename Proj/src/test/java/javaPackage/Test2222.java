57494edaac2dd4a9a5f534994df67bced2a05815
KHUSHI SHAH <KHUSHI SHAH@LAPTOP-ETMNRLTD>
2025-07-24 10:24:26 +0530

    Final changes

---
+++ Proj/src/test/java/javaPackage/Test2222.java
@@ -0,0 +1,12 @@
+package javaPackage;
+
+public class Test2222 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Second changes are done");
+
+	}
+
+}
